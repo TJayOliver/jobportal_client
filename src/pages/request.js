0d2a79e07690b8d5a52ca83d4ade1abba739bc09
T-Jay Oliver <tjoliver1@yahoo.com>
2024-05-04 03:18:31 +0000

    change added

--- src/pages/request.js
+++ src/pages/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:4040";
+export const BASE_URL = "https://futureforteserver.onrender.com";
 
 export const fetch = async (
   link,
