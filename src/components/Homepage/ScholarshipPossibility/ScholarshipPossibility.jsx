ca9dc27004424f7e5fdccef2024b1b16defbe212
T-Jay Oliver <126061191+TJayOliver@users.noreply.github.com>
2024-05-04 00:10:31 -0800

    Update ScholarshipPossibility.jsx

--- src/components/Homepage/ScholarshipPossibility/ScholarshipPossibility.jsx
+++ src/components/Homepage/ScholarshipPossibility/ScholarshipPossibility.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import image from "../../../assets/Cyril.jpg";
+import image from "../../../assets/cyril.jpg";
 
 const ScholarshipPossibility = () => {
   return (
