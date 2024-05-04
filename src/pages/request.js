e04608d185640d70126e510fe79eed1a9064d1fb
T-Jay Oliver <126061191+TJayOliver@users.noreply.github.com>
2024-05-04 06:23:11 -0800

    Update request.js

--- src/pages/request.js
+++ src/pages/request.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://futureforte.vercel.app";
+export const BASE_URL = "https://futurebackend.netlify.app";
 
 export const fetch = async (
   link,
