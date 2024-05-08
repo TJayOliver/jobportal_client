d004651d579e8276a641a29c6b14a85ef63f9dcf
TJayOliver <tjoliver1@yahoo.com>
2024-05-08 12:00:09 -0800

    platforms changes

--- src/components/Platforms/Platforms.jsx
+++ src/components/Platforms/Platforms.jsx
@@ -2,8 +2,8 @@ import whatsAppImage from "../../assets/whatsApp.png";
 
 const Platforms = () => {
   return (
-    <aside className="bottom-48 left-3 rounded-lg h-24 w-24 fixed drop-shadow-md">
-      <a href="bit.ly/4dxzY0S">
+    <aside className="rounded-full left-3 h-16 w-16 fixed bottom-14 drop-shadow-md z-50">
+      <a href="https://bit.ly/4dxzY0S">
         <img src={whatsAppImage} className="w-full h-full object-cover" />
       </a>
     </aside>
